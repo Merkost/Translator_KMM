afed0125a3f8a543d8afeaa9f513725d3afb409e
merkost <konstantinlikes@gmail.com>
2023-01-25 13:36:02 +1100

    Translate viewModels with Event and Item classes

---
+++ shared/src/commonMain/kotlin/com/merkost/translator_kmm/translate/presentation/UiHistoryItem.kt
@@ -0,0 +1,12 @@
+package com.merkost.translator_kmm.translate.presentation
+
+import com.merkost.translator_kmm.core.presentation.UiLanguage
+
+
+data class UiHistoryItem(
+    val id: Long,
+    val fromText: String,
+    val toText: String,
+    val fromLanguage: UiLanguage,
+    val toLanguage: UiLanguage
+)
